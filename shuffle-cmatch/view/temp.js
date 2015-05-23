44b2aaa4dfa8a875e3cd0776f2be5781c646ab78
3ygun <soller.23@osu.edu>
2015-05-22 21:20:50 -0400

    Added score files & deleted temp.

--- shuffle-cmatch/view/temp.js
+++
@@ -1,8 +0,0 @@
-function showCMatchMelee2(data) {
-	// Update the text
-	$matchTitle.html("" + data.title.toUpperCase());
-    $leftName.html("" + data.leftName.toUpperCase());
-    $rightName.html("" + data.rightName.toUpperCase());
-	
-	// Space text
-}
